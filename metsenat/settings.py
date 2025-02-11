902e76766c29bfbbc87849d32ba54136fcd3bf51
johnibek <jonibekhamroqulov2004@gmail.com>
2025-02-11 15:00:54 +0500

    settings changed

--- metsenat/settings.py
+++ metsenat/settings.py
@@ -119,8 +119,8 @@ DATABASES = {
     }
 }
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+# if 'DATABASE_URL' in os.environ:
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
