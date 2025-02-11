c2123efb0ceddfdd7e611c61f9a869b12f92c139
johnibek <jonibekhamroqulov2004@gmail.com>
2025-02-11 17:19:44 +0500

    access token lifetime changed

--- metsenat/settings.py
+++ metsenat/settings.py
@@ -73,7 +73,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
 }
 
